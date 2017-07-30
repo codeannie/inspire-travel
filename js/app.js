37c0ab307f68f1d9edceca7168d836852428f3c5
Annie Chiang <codepanduh@gmail.com>
2017-07-30 16:13:15 -0700

    fixed google timezone url & got successful response

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 const GOOGLE = {
     places_url : "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
-    timezone_url : "https://maps.googleapis.com/maps/api/timezone/json",
+    timezone_url : "https://maps.googleapis.com/maps/api/timezone/json?",
     key : "AIzaSyDWmnY0GfjllGfQUwp5ytwEcXudmS6axEo",
 };
 
@@ -46,12 +46,13 @@ function initPlaces(inputElem) {
     var autocomplete = new google.maps.places.Autocomplete(inputElem, {types: ['(cities)']});
     autocomplete.addListener('place_changed', function () {
         STATE.googlePlace = autocomplete.getPlace();
-        console.log(STATE.googlePlace);
+        // console.log(STATE.googlePlace);
         STATE.geoLat = STATE.googlePlace.geometry.location.lat();
         STATE.geoLng = STATE.googlePlace.geometry.location.lng();
         STATE.cityName = STATE.googlePlace.formatted_address;
-        console.log(STATE.cityName);
+        // console.log(STATE.cityName);
         getCityData();
+        getLocationTime();
     })
 }
 
@@ -68,49 +69,68 @@ function getCityData() {
         .then(getForecastData);
 }
 
-function getForecastData(json) {
-    STATE.cityKey = json.Key;  
+function getForecastData(json_weather) {
+    STATE.cityKey = json_weather.Key;  
 
     let param = {
         apikey: ACCUWEATHER.key,
     }
 
     $.getJSON(ACCUWEATHER.forecast_url + STATE.cityKey, param)
-    .then (function (json) {
-        console.log(json)
+    .then (function (json_weather) {
+    var data = json_weather
+    
+    //render parts needed from that variable/object
+    //using angular with {{}} <--this is more recommended & easier 
+    //set up HTML variables and use as syntax 
+        //data.celsius temp 
+
+    //jquery $(<insert class>) - to get
+        //more laborious
+    // console.log(json)
     })
 }
 
 //display Forecast into boxes - 1 day? 3 day? 
-function weatherHTML() {
+// function weatherHTML() {
+// }
 
-}
+//use ANGULAR 
 
-// USER LOCAL TIME
+// GET USER TIME
 function getUserTime () {
     var date = moment().format("MMMM Do YY, h:mm:ss a");
-    console.log(date);
+    // console.log(date);
 }
-// LOCATION TIME
-// function getLocationTime () {
-//     let param = {
-//         q: 
-//     }
-// }
+
+// GET SUBMITTED LOCATION TIME
+function getLocationTime () {
+    var targetDate = new Date()
+    var daysofweek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']
+    let param = {
+        location: `${STATE.geoLat},${STATE.geoLng}`,
+        timestamp: targetDate.getTime()/1000 + targetDate.getTimezoneOffset() * 60,
+        key: GOOGLE.key, 
+    }
+    $.getJSON(GOOGLE.timezone_url, param)
+    .then(function(json_time) {
+     console.log(json_time);
+    })
+}
 // PHOTOS
 
-//research AJAX $(document).ready() - line 69 and on is short hand 
+//research AJAX $(document).ready()
 //don't load line 69~ javascript until DOM is ready
 //for DOM related functions 
-
 $(function(){
 const inputElem = $('.js-searchLocation')[0];
 initPlaces(inputElem);
 userSubmit();
 getUserTime();
+// getLocationTime();
 // getCityData();
 // getForecastData();
-})
+});
 
 // https://github.com/getify/You-Dont-Know-JS/blob/master/async%20%26%20performance/ch3.md
 // Doug Mason & Russel Thomas - Promises 
