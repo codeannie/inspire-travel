f7d57ecc5db96ced748502bc44a0f760e63ef9d2
Annie Chiang <codepanduh@gmail.com>
2017-08-01 11:06:28 -0700

    attempting to get respones from flickr api - json

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const ACCUWEATHER = {
 const FLICKR = {
     // url: "https://flickr.photos.geo.photosForLocation",
     // url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&",
-    url: "https://api.flickr.com/services/",
+    url: "https://api.flickr.com/services/flickr.photos.search/",
     // url: "https://api.flickr.com/services/flickr.photos.search/json/",
     key: "7faa8e131c5e122b2c8641a0f601cef6",
     secret: "b37b20e78b51a47b",
@@ -136,64 +136,32 @@ function getLocationTime () {
             // STATE.cityTime = moment.tz(param.timestamp, timeZone).format("YYYY-MM-DD hh:mm:ss a");
             // STATE.cityTime = moment.tz(param.timestamp, timeZone).format("ha z");
 
-            // let timeHTML = 
-            //     `{<div class="time-box">Location Time: <span id="location-time">${cityTime}</span></div>}`
-           
             // console.log(STATE.cityTime);
             $("#location-time").text(STATE.cityTime); //how to format this output?
         })
         // .append(timeHTML);
 }
-
+    
 // PHOTOS
 function getPhotoData() {
-    $.ajax({
-        url: FLICKR.url,
-        data: {
-            method: "flickr.photos.search",
-            lat: `${STATE.geoLat}`,
-            lon: `${STATE.geoLng}`,
-            api_key: FLICKR.key,
-            privacy_filter: 1,
-            safe_search: 1, 
-            per_page: 10,
-            page: 2,
-            format: "json", 
-            nojsoncallback: 1
-        },
-        
-        success: function(response) {
-            $.each(response.photos.photo, function (index,value) {
-                console.log(value);
-                var url = 'https://farm' + value.farm + '.staticflickr.com/' + value.server + '/' + value.id + '_' + value.secret + '.jpg';
-                var a = $('<a>').attr({href: url})
-                var img = $('img').attr({src: url})
-                a.append(img);
-                $("#image-gallery").append(a);
-            });
-            $("#image-gallery").justifiedGallery();
-        }
-    });
-}
+    let param = {
+        lat: `${STATE.geoLat}`,
+        lon: `${STATE.geoLng}`,
+        api_key: FLICKR.key,
+        privacy_filter: 1,
+        safe_search: 1, 
+        per_page: 10,
+        page: 3, 
+    }
 
-    // let param = {
-    //     lat: `${STATE.geoLat}`,
-    //     lon: `${STATE.geoLng}`,
-    //     api_key: FLICKR.key,
-    //     privacy_filter: 1,
-    //     safe_search: 1, 
-    //     per_page: 10,
-    //     page: 3, 
-    // }
-
-    // $.getJSON(FLICKR.url, param)
-    //     .then(function(json_photos) {
-            // alert("Photos!");
-            // console.log(json_photos);
-            // for (var i=0, i<json_photos.length, i++) {
+    $.getJSON(FLICKR.url, param)
+        .then(function(json_photos) {
+            alert("Photos!");
+            console.log(json_photos);
+            // for (var i=0; i<json_photos.length; i++) {
             // }
-//         })
-// }
+        })
+}
 
 // function displayPhotos() {
 //     let photoHTML = {}
