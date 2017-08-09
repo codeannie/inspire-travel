8f115e378baa64202782117db19bfd03bf86972b
Annie Chiang <codepanduh@gmail.com>
2017-08-08 18:02:35 -0700

    changed # of photos returned

--- js/app.js
+++ js/app.js
@@ -179,7 +179,7 @@ function getPhotoData() {
         nojsoncallback: 1,
         privacy_filter: 1,
         safe_search: 1, 
-        per_page: 8,
+        per_page: 9,
     }
     $.getJSON(FLICKR.url, param)
         .then(function(json_photos) {
